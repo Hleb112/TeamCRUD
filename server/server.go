616d74dd08775ed4300ebaf39ddb9c6e03e6c4cd
Hromiken <hromiken@gmail.com>
2025-04-23 18:59:03 +0300

    Gleb smotri

--- server/server.go
+++ server/server.go
@@ -13,7 +13,20 @@ func CreateServer() {
 		// define some specific configuration
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
+		Handler:      &myHandler{},
 	}
 	// launch the server
 	log.Fatal(myServer.ListenAndServe())
+}
+
+type myHandler struct {
+}
+
+// function executed for each HTTP request received
+func (h *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	toSend := []byte("<html><head>Всем питухам</head><body>Здарова</hello></html>")
+	_, err := w.Write(toSend)
+	if err != nil {
+		log.Printf("error while writing on the body %s", err)
+	}
 }
